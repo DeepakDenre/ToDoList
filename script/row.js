41dff77374d23f9ff074ec0691eb3c00267babf4
DeepakDenre <denredeepak@gmail.com>
2024-03-10 13:57:06 +0530

    Add logo

--- script/row.js
+++ script/row.js
@@ -2,26 +2,25 @@
 let count = 0;
 
 const row = document.getElementById('row');
+row.style.display = 'block';
 
 function addRow(n = 10){
     setTimeout(
         ()=>{
             while(count <= 500){
-                for(let a=0;a<=n;a++){
+                for(let a=0; a<=n; a++){
                     const new_row = document.createElement('tr');
                     const cell1 = document.createElement('td');
                     const cell2 = document.createElement('td');
                     const cell3 = document.createElement('td');
                     const cell4 = document.createElement('td');
+
                     cell1.className = 'No';
                     cell2.className = 'Task';
                     cell3.className = 'DueDate';
                     cell4.className = 'Completed';
 
-                    cell1.innerHTML = count+")";
-                    cell2.innerHTML = "Row"+count;
-                    cell3.innerHTML = "20/03/2024";
-                    cell4.innerHTML = "No";
+                    cell1.innerHTML = count;
 
                     new_row.appendChild(cell1);
                     new_row.appendChild(cell2);
