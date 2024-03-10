64d5a4c3a60460410d68221ae67972134d3dcda7
Deepak Denre <denredeepak@gmail.com>
2024-03-10 19:55:30 +0530

    Refactor table layout and input styles

--- script/row.js
+++ script/row.js
@@ -1,13 +1,12 @@
 
 let count = 0;
 
-const row = document.getElementById('row');
-row.style.display = 'block';
+const row = document.getElementById('content-table');
 
 function addRow(n = 10){
     setTimeout(
         ()=>{
-            while(count <= 500){
+            while(count <= 100){
                 for(let a=0; a<=n; a++){
                     const new_row = document.createElement('tr');
                     const cell1 = document.createElement('td');
@@ -15,12 +14,19 @@ function addRow(n = 10){
                     const cell3 = document.createElement('td');
                     const cell4 = document.createElement('td');
 
+                    cell1.id = 'No'+count;
+                    cell2.id = 'Task'+count;
+                    cell3.id = 'DueDate'+count;
+                    cell4.id = 'Completed'+count;
+
+                    cell2.innerHTML = "<input type='text' class='task-input'>";
+                    cell3.innerHTML = "<input type='date' class='date-input'>";
+                    cell4.innerHTML = "<input type='checkbox' class='completed-input'>";
+
                     cell1.className = 'No';
                     cell2.className = 'Task';
                     cell3.className = 'DueDate';
                     cell4.className = 'Completed';
-
-                    cell1.innerHTML = count;
 
                     new_row.appendChild(cell1);
                     new_row.appendChild(cell2);
